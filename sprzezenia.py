8e9bdc0121b49a314f6536af0620f7a1eaa804b9
K0034 <102217767+K0034@users.noreply.github.com>
2023-05-11 15:27:03 +0200

    Update sprzezenia.py

--- sprzezenia.py
+++ sprzezenia.py
@@ -1,1 +1,10 @@
+#bodziec(S) - oddzialywanie wejsciowe systemu
+#reakcja(R) - oddzialywanie wyjsciowe systemu
+#reaktywnosc(r) - stosunek reakcji do bodzcca
+r=R/S
 
+#y1 reakcja systemu Y na bodziec od X
+y1=ry*x1
+
+#x2 reakcja systemu X na bodziec y1
+x2=rx*y1
